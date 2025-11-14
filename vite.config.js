1f2197012c64abf69f4a844457c9035aed09d185
0xRyuu <329454672@qq.com>
2025-11-14 11:21:17 +0800

    调试并修复使用Vite带来的bug

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   // 项目根目录，Vite 将在这里寻找 index.html
@@ -16,9 +17,20 @@ export default defineConfig({
     proxy: {
       // 将所有 /api 开头的请求代理到后端服务
       '/api': {
-        target: 'http://127.0.0.1:3000', // 后端服务器地址
+        target: 'http://127.0.0.1:5000', // 后端服务器地址
         changeOrigin: true, // 必须设置为 true，以正确处理代理
       },
     },
   },
+
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'locales',
+          dest: '.'
+        }
+      ]
+    })
+  ]
 });
